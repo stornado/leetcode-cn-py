f6dc509da85d8312685b0393c34054fcf8198f56
仲夏叶 <stornado@qq.com>
2019-09-08 14:24:17 +0800

    两数相加

---
+++ 2.两数相加.py
@@ -0,0 +1,67 @@
+#
+# @lc app=leetcode.cn id=2 lang=python3
+#
+# [2] 两数相加
+#
+# https://leetcode-cn.com/problems/add-two-numbers/description/
+#
+# algorithms
+# Medium (35.49%)
+# Likes:    2959
+# Dislikes: 0
+# Total Accepted:    195.7K
+# Total Submissions: 551.4K
+# Testcase Example:  '[2,4,3]\n[5,6,4]'
+#
+# 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+#
+# 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+#
+# 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+#
+# 示例：
+#
+# 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+# 输出：7 -> 0 -> 8
+# 原因：342 + 465 = 807
+#
+#
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        i, j, value = 0, 0, 0
+        while l1.next is not None:
+            value += pow(10, i) * l1.val
+            l1 = l1.next
+            i += 1
+        value += pow(10, i) * l1.val
+
+        while l2.next is not None:
+            value += pow(10, j) * l2.val
+            l2 = l2.next
+            j += 1
+        value += pow(10, j) * l2.val
+
+        value_bit = []
+        if value == 0:
+            value_bit = [0]
+        elif value > 0:
+            m = value
+            while m != 0:
+                value_bit.append(m % 10)
+                m //= 10
+            value_bit.reverse()
+        node = None
+        l = None
+        for b in value_bit:
+            l = ListNode(b)
+            l.next = node
+            node = l
+        return l
